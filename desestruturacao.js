4e29d566f27f6a465ea070e06e2797404f1f11de
ebagabe <gabriel.ebagabe@gmail.com>
2024-06-22 18:11:45 -0300

    Desestruturacao

---
+++ desestruturacao.js
@@ -0,0 +1,14 @@
+// objetos
+const pessoa = {
+    nome: 'Gabe',
+    idade: 22,
+    stack: "Backend"
+};
+
+const { nome, idade, stack } = pessoa;
+console.log(nome, idade, stack);
+
+// arrays
+const idades = [10, 20, 30, 50, 70];
+
+const [joao, maria, jorge, ze, margarida] = idades;
